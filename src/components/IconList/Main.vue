4911a0c38c70f594dc0e324c8c88f936c07a8ec1
konginator <69498235+konginator@users.noreply.github.com>
2023-02-17 15:38:33 -0600

    added icon lists

--- src/components/IconList/Main.vue
+++ src/components/IconList/Main.vue
@@ -1,8 +1,11 @@
 <template>
     <ul class="list">
-        <li>
-            <a href="/">
-                <font-awesome-icon icon="fa-brands fa-github" size="lg" />
+        <li class="list-item" v-for="item in sites" v-bind:key="item">
+            <a v-bind:href="item.url">
+                <font-awesome-icon
+                v-bind:icon="item.icon"
+                class="icon"
+            />
             </a>
         </li>
     </ul>
@@ -11,6 +14,16 @@
 <style scoped>
     .list {
         list-style: none;
+        display: flex;
+        align-items: center;
+        margin: 0;
+        padding: 0;
+        gap: 1rem;
+    }
+
+    .icon {
+        font-size: 1.8rem;
+        color: #303030;
     }
 </style>
 
@@ -20,6 +33,18 @@ export default {
     name: 'IconList',
     data () {
         return {
+            sites: [
+                {
+                    id: 1,
+                    icon: 'fa-brands fa-github',
+                    url: 'https://github.com/brandon-kong/better-weather-app'
+                },
+                {
+                    id: 2,
+                    icon: 'fa-brands fa-twitter',
+                    url: 'https://google.com'
+                }
+            ],
             msg: 'Welcome to Your Vue.js App'
         }
     },
