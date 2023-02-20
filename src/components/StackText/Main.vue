62a525658c05e2930f55a12cfedd909d4e4b816b
konginator <69498235+konginator@users.noreply.github.com>
2023-02-19 23:32:47 -0600

    changed new

--- src/components/StackText/Main.vue
+++ src/components/StackText/Main.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="stack-container">
         <div class="stack-status">New</div>
-        <h1 class="stack-title"><strong>Weather</strong></h1>
+        <h1 class="stack-title">Weather</h1>
         <h2 class="stack-subtitle">Like you've never seen before</h2>
         <p class="stack-text">Get extensive weather reports for any place in the world!</p>
     </div>
@@ -25,31 +25,32 @@
     }
 
     .stack-status {
-        padding: .1rem 1rem;
+        padding: .1rem 0rem;
         border-radius: 30px;
-        font-size: 1rem;
-        font-weight: 400;
-        color: #fff;
+        font-size: 1.3git addrem;
+        font-weight: 600;
+        color: #a24ab0;
         background-color: #a24ab0;
+        background-color: white;
     }
 
     .stack-title {
         font-size: 2.5rem;
-
+        font-weight: 500;
         color: #000;
-        line-height: 1.3;
+        line-height: 1.1;
     }
 
     .stack-subtitle {
-        font-size: 1.8rem;
-        font-weight: 400;
+        font-size: 1.5rem;
+        font-weight: 600;
         color: #a4a4a4;
         line-height: 1.1;
     }
 
     .stack-text {
-        font-size: 1.2rem;
-        font-weight: 400;
+        font-size: 1.3rem;
+        font-weight: 300;
         color: #a4a4a4;
         line-height: 1.2;
     }
